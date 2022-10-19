e5977a2e4147adb5553e4083c23cfa1ecb757ef4
Ian Ker-Seymer <hello@ianks.com>
2022-10-19 18:14:18 -0400

    Test the builds

---
+++ Rakefile
@@ -0,0 +1,6 @@
+namespace :build do
+  desc "Simulate GitHub action build locally"
+  task :local do
+    sh "act push --container-architecture linux/amd64 --rm"
+  end
+end
---
+++ test/tests.rb
@@ -0,0 +1,26 @@
+require "rbconfig"
+
+RB = RbConfig::CONFIG
+
+def it(name, &blk)
+  $stderr.printf "Running #{name.inspect}... "
+  blk.call
+  warn "OK\n"
+rescue => e
+  puts "FAIL: #{name}"
+  raise e
+end
+
+def assert_equal(a, b)
+  raise "Expected #{a.inspect} to equal #{b.inspect}" unless a == b
+end
+
+it "is compiled for the expected architecture" do
+  assert_equal(ENV["EXPECTED_RUBY_ARCH"], RB["arch"])
+end
+
+it "is the expected version of ruby" do
+  assert_equal(ENV["RUBY_VERSION"], [RB["MAJOR"], RB["MINOR"], RB["TEENY"]].join("."))
+end
+
+warn "✅ All tests passed!"
