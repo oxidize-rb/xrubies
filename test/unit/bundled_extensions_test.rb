07e4c7214dbb944725a665b54971ff0b780c8d25
Ian Ker-Seymer <hello@ianks.com>
2022-12-30 17:34:29 -0500

    Include readline, add tests for more extensions

--- test/unit/bundled_extensions_test.rb
+++ test/unit/bundled_extensions_test.rb
@@ -16,6 +16,9 @@ describe "Precompiled Ruby" do
   it "works with bundled compiled gems (openssl)" do
     require "openssl"
 
+    result = OpenSSL::HMAC.hexdigest("SHA256", "key", "The quick brown fox jumps over the lazy dog")
+
+    assert_equal("f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8", result)
   end
 
   it "works with bundled compiled gems (zlib)" do
@@ -31,5 +34,18 @@ describe "Precompiled Ruby" do
   it "works with bundled compiled gems (readline)" do
     require "readline"
 
+    len, wid = Readline.get_screen_size
+
+    assert_kind_of(Integer, len)
+    assert_kind_of(Integer, wid)
+  end
+
+  it "works with bundled compiled gems (fiddle)" do
+    require "fiddle"
+
+    libc = Fiddle.dlopen("libc.#{RbConfig::CONFIG["SOEXT"]}")
+    strlen = Fiddle::Function.new(libc["strlen"], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
+
+    assert_equal(5, strlen.call("hello"))
   end
 end
