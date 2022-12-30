d19fcd0c076a08fbf51f39167f307bbec259c4fc
Ian Ker-Seymer <hello@ianks.com>
2022-12-30 16:01:13 -0500

    Fix openssl install

--- docker/pkg/openssl.sh
+++ docker/pkg/openssl.sh
@@ -3,7 +3,7 @@
 
 current_dir="$(dirname "${BASH_SOURCE[0]}")"
 
-if [[ $(echo "$RUBY_MINOR >= 3.2" | bc) == 1 ]] ; then
+if [ "$(echo "$RUBY_MINOR" | awk '{print ($1 >= "3.2") ? "true" : "false"}')" = "true" ]; then
   source "$current_dir/openssl_3.sh"
 else
   source "$current_dir/openssl_1_1.sh"
