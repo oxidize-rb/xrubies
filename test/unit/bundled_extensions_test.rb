443d91b5701a61397e48fca67ae24659509edb5f
Ian Ker-Seymer <hello@ianks.com>
2022-12-30 17:48:13 -0500

    Compile mini lib

--- test/unit/bundled_extensions_test.rb
+++ test/unit/bundled_extensions_test.rb
@@ -1,4 +1,8 @@
 require "test_helper"
+require "rbconfig"
+require "tmpdir"
+
+RB = RbConfig::CONFIG
 
 describe "Precompiled Ruby" do
   it "works with bundled compiled gems (yaml)" do
@@ -19,6 +23,12 @@ describe "Precompiled Ruby" do
     result = OpenSSL::HMAC.hexdigest("SHA256", "key", "The quick brown fox jumps over the lazy dog")
 
     assert_equal("f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8", result)
+
+    require "net/http"
+
+    body = Net::HTTP.get(URI("https://www.ruby-lang.org/en/"))
+
+    assert_match(/Ruby/, body)
   end
 
   it "works with bundled compiled gems (zlib)" do
@@ -43,10 +53,28 @@ describe "Precompiled Ruby" do
   it "works with bundled compiled gems (fiddle)" do
     require "fiddle"
 
-    libc = Fiddle.dlopen("libc.#{RbConfig::CONFIG["SOEXT"]}")
-    strlen = Fiddle::Function.new(libc["strlen"], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
+    Dir.mktmpdir do |dir|
+      path = File.join(dir, "test.so")
+      c_file = File.join(dir, "test.c")
 
-    assert_equal(5, strlen.call("hello"))
-    assert_equal(2, strlen.call("he\0llo"))
+      File.write(
+        c_file,
+        <<~C
+          int lolstrlen(const char *s) {
+            int len = 0;
+            while (*s++) { len++; }
+            return len;
+          }
+        C
+      )
+
+      system("#{RB["LDSHARED"]} -o #{path} #{c_file}")
+
+      mylib = Fiddle.dlopen(path.to_s)
+      strlen = Fiddle::Function.new(mylib["lolstrlen"], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
+
+      assert_equal(5, strlen.call("hello"))
+      assert_equal(2, strlen.call("he\0llo"))
+    end
   end
 end
